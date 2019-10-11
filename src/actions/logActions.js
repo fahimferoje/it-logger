a9ffdbd2fcb59b7db90e3f60507225d3f10d3c92
fahimferoje <fabrahamjohn@gmail.com>
2019-10-11 12:25:03 +0600

    Sec 12 ep 83: connecting redux to component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,30 +1,26 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
-import { async } from 'q';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
 export const getLogs = () => async (dispatch) => {
+	try {
+		setLoading();
 
-    try {
+		const res = await fetch("/logs");
+		const data = await res.json();
 
-        setLoading();
-
-	const res = await fetch("/logs");
-	const data = await res.json();
-
-	dispatch({
-		type: GET_LOGS,
-		payload: data
-	});
-        
-    } catch (err) {
-        dispatch({
-            type: LOGS_ERROR,
-            payload: err.response.data
-        });
-    }
+		dispatch({
+			type: GET_LOGS,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
 };
 
 export const setLoading = () => {
-    return {
-        type: SET_LOADING
-    }
-}
+	return {
+		type: SET_LOADING
+	};
+};
